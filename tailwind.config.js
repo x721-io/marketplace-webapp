042c0274f45f4acdc8fd598d886c0080ca41708a
vietthai244 <thai.toviet@gmail.com>
2023-11-13 14:37:38 +0700

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const { colors } = require('./config/theme')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,10 +9,32 @@ module.exports = {
     './node_modules/flowbite-react/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: '480px',
+        tablet: '768px',
+        desktop: '1280px'
+      },
+      colors: {
+        ...colors,
+        primary: colors.gray[800],
+        secondary: colors.gray[600],
+        tertiary: colors.gray[400],
+        disabled: colors.gray[200]
+      },
+      backgroundColor: {
+        surfaceSoft: colors.gray[100],
+        surfaceMedium: colors.gray[200],
+        surfaceHard: colors.gray[500],
+        button: colors.gray[900],
+        buttonSecondary: colors.gray[100],
+        tab: colors.gray[0]
+      },
+      fontSize: {}
+    }
   },
   plugins: [
     require('flowbite/plugin')
-  ],
+  ]
 }
 
