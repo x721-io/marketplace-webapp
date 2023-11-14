c6250694eb6f24b18266877cfce8807d0ddae0f7
vietthai244 <thai.toviet@gmail.com>
2023-11-14 14:38:48 +0700

    Update Icon component

--- components/Icon/burger.tsx
+++ components/Icon/burger.tsx
@@ -4,7 +4,9 @@ import { classNames } from '@/utils/string'
 export default function BurgerIcon({ width, height, color, className }: IconProps) {
   return (
     <svg
-      className={classNames('w-5 h-5', `text-${color}`, className)}
+      width={width}
+      height={height}
+      className={classNames(`text-${color}`, className)}
       aria-hidden="true"
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
