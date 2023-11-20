13461ca6a15707604fd02c6b56b5dc68f7f52cde
Kim Chi Nguyen <chinpk@uniultra.xyz>
2023-11-20 15:33:12 +0700

    fix tabs in profile page and explore user

--- config/flowbite.ts
+++ config/flowbite.ts
@@ -3,9 +3,10 @@ import { CustomFlowbiteTheme } from 'flowbite-react'
 const tab: CustomFlowbiteTheme['tab'] = {
   base: 'flex flex-col gap-2 block',
   tablist: {
-    base: 'inline-flex text-center p-1',
+    base: 'inline-flex text-center p-1 w-full',
     styles: {
-      default: 'flex-wrap bg-surface-soft rounded-2xl'
+      default: 'flex-wrap bg-surface-soft rounded-2xl',
+      underline: 'flex-wrap border-b border-gray-200 p-0'
     },
     tabitem: {
       base: 'flex items-center justify-center px-4 py-3 text-body-14 font-medium first:ml-0 disabled:cursor-not-allowed focus:outline-none',
@@ -16,7 +17,14 @@ const tab: CustomFlowbiteTheme['tab'] = {
             on: 'bg-white text-primary shadow',
             off: 'text-secondary bg-transparent'
           }
-        }
+        },
+        underline: {
+          base: 'text-sm',
+          active: {
+            on: 'text-primary border-b-2 border-primary',
+            off: 'text-secondary bg-transparent'
+          }
+        },
       }
     }
   }
