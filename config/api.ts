e0dad4cf9bff72161e80d919150a33720e58763d
vietthai244 <thai.toviet@gmail.com>
2023-11-30 18:08:32 +0700

    Fix api url

--- config/api.ts
+++ config/api.ts
@@ -1,4 +1,4 @@
-export const BASE_API_URL = process.env.NODE_ENV === 'development' ? 'https://marketplace-api-dev.uniultra.xyz' : ''
+export const BASE_API_URL = 'https://marketplace-api-dev.uniultra.xyz'
 // export const BASE_API_URL = process.env.NODE_ENV === 'development' ? 'https://b10g0wn1-8888.asse.devtunnels.ms' : ''
 
 export const API_ENDPOINTS = {
