3b122e6d92b8857c306f2467114c7c6d536ac7b2
vietthai244 <thai.toviet@gmail.com>
2023-12-20 17:19:41 +0700

    Update allowed file extensions

--- config/constants.ts
+++ config/constants.ts
@@ -12,5 +12,5 @@ export const FINGERPRINT = '0x00000000000000000000000000000000000000000000000000
 
 export const ALLOWED_IMAGE_TYPES = ".png,.jpeg, .png, .gif, .webp"
 export const ALLOWED_AUDIO_TYPES = ".mp3"
-export const ALLOWED_VIDEO_TYPES = ".mp4"
+export const ALLOWED_VIDEO_TYPES = ".mp4, .webm"
 export const ALLOWED_FILE_TYPES = [ALLOWED_IMAGE_TYPES, ALLOWED_AUDIO_TYPES, ALLOWED_VIDEO_TYPES].join(', ')
