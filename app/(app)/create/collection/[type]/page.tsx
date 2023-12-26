c9354b56e87a3901e12fb01d21e35112a1ccac13
vietthai244 <thai.toviet@gmail.com>
2023-12-26 15:20:46 +0700

    Refactor create section route structures => fix bug #97

---
+++ app/(app)/create/collection/[type]/page.tsx
@@ -0,0 +1,274 @@
+'use client'
+
+import Text from '@/components/Text'
+import ImageUploader from '@/components/Form/ImageUploader'
+import Input from '@/components/Form/Input'
+import Textarea from '@/components/Form/Textarea'
+import Button from '@/components/Button'
+import { Controller, useForm } from 'react-hook-form'
+import { useEffect, useState } from 'react'
+import {
+  useCreateCollection,
+  useUpdateCollection
+} from '@/hooks/useCollection'
+import { randomWord } from '@rarible/types'
+import useAuthStore from '@/store/auth/store'
+import { BASE_API_URL } from '@/config/api'
+import Icon from '@/components/Icon'
+import { toast } from 'react-toastify'
+import { AssetType } from '@/types'
+import ConnectWalletButton from '@/components/Button/ConnectWalletButton'
+import { useMarketplaceApi } from '@/hooks/useMarketplaceApi'
+import FormValidationMessages from '@/components/Form/ValidationMessages'
+import { noSpecialCharacterRegex } from '@/utils/regex'
+import { parseImageUrl } from '@/utils/nft'
+import { waitForTransaction } from '@wagmi/core'
+import { redirect, useParams, useRouter } from 'next/navigation'
+
+interface CollectionFormState {
+  avatar: string
+  name: string,
+  symbol: string,
+  description: string,
+  shortUrl: string
+}
+
+export default function CreateNFTCollectionPage() {
+  const type = useParams().type.toString().toUpperCase() as AssetType
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const [validating, setValidating] = useState(false)
+  const [uploading, setUploading] = useState(false)
+  const api = useMarketplaceApi()
+  const creator = useAuthStore(state => state.profile?.id)
+  const { onCreateCollectionContract } = useCreateCollection()
+  const { onCreateCollection } = useUpdateCollection()
+  const {
+    handleSubmit,
+    register,
+    reset,
+    watch,
+    setError,
+    clearErrors,
+    control,
+    setValue,
+    formState: { errors }
+  } = useForm<CollectionFormState>({ reValidateMode: 'onChange' })
+
+  const formRules = {
+    name: {
+      required: 'Collection name is required!'
+    },
+    symbol: {
+      required: 'Symbol is required!',
+      pattern: { value: noSpecialCharacterRegex, message: 'Collection symbol should not contain special characters' }
+    },
+    shortUrl: {
+      pattern: { value: noSpecialCharacterRegex, message: 'Short url should not contain special characters' }
+    },
+    description: {
+      maxLength: { value: 256, message: 'Description cannot exceed 256 characters' }
+    },
+    image: {
+      required: 'Collection image is required!'
+    }
+  }
+
+  const handleUploadImage = async (file?: Blob) => {
+    if (!file) {
+      setValue('avatar', '')
+      return
+    }
+    setUploading(true)
+    try {
+      await toast.promise(api.uploadFile(file), {
+        pending: 'Uploading image...',
+        success: {
+          render: (data) => {
+            setValue('avatar', parseImageUrl(data.data?.fileHashes[0]) as string)
+            return 'Collection image uploaded successfully'
+          }
+        },
+        error: {
+          render: (error) => {
+            setValue('avatar', '')
+            return `Uploading error: ${(error.data as any).message}`
+          }
+        }
+      })
+    } finally {
+      setUploading(false)
+    }
+  }
+
+  const resetForm = () => {
+    reset()
+    router.push('/create/collection')
+  }
+
+  const onSubmit = async (data: CollectionFormState) => {
+    if (!type || !creator) return
+    const toastId = toast.loading('Preparing data...', { type: 'info' })
+    setLoading(true)
+    try {
+      const salt = randomWord()
+      const fullShortUrl = BASE_API_URL + '/collection/' + data.shortUrl
+      const args = [data.name, data.symbol, `ipfs://`, fullShortUrl, [], salt]
+
+      toast.update(toastId, { render: 'Sending transaction', type: 'info' })
+
+      const tx = await onCreateCollectionContract(type, args)
+      await Promise.all([
+        waitForTransaction({ hash: tx.hash }),
+
+        onCreateCollection({
+          ...data,
+          type,
+          txCreationHash: tx.hash,
+          creators: creator
+        })
+      ])
+
+      toast.update(toastId, {
+        render: 'Collection created successfully',
+        type: 'success',
+        isLoading: false,
+        autoClose: 5000
+      })
+      resetForm()
+      router.push('/explore/collections')
+    } catch (e: any) {
+      toast.update(toastId, {
+        render: `Error creating collection: ${e.message ?? e}`,
+        type: 'error',
+        isLoading: false,
+        autoClose: 5000
+      })
+      console.error(e)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleValidateInput = async (name: string, value: Record<string, any>) => {
+    try {
+      setValidating(true)
+      if (name === 'name' && !!value.name) {
+        const existed = await api.validateInput({ key: 'collectionName', value: value.name })
+        if (existed) setError('name', { type: 'custom', message: 'Collection name already existed' })
+        else clearErrors('name')
+      }
+
+      if (name === 'symbol' && !!value.symbol) {
+        const existed = await api.validateInput({ key: 'collectionSymbol', value: value.symbol })
+        if (existed) setError('symbol', { type: 'custom', message: 'Collection symbol already existed' })
+        else clearErrors('symbol')
+      }
+
+      if (name === 'shortUrl' && !!value.shortUrl) {
+        const existed = await api.validateInput({ key: 'collectionShortUrl', value: value.shortUrl })
+        if (existed) setError('shortUrl', { type: 'custom', message: 'Short url already existed' })
+        else clearErrors('shortUrl')
+      }
+    } finally {
+      setValidating(false)
+    }
+  }
+
+  useEffect(() => {
+    const subscription = watch(async (value, { name, type }) => {
+      if (!name) return
+      handleValidateInput(name, value)
+    })
+    return () => subscription.unsubscribe()
+  }, [watch])
+
+  useEffect(() => {
+    if (type !== 'ERC1155' && type !== 'ERC721') {
+      return redirect('/create/collection')
+    }
+  }, [type]);
+
+  return (
+    <div className="w-full flex justify-center py-10 tablet:py-20 desktop:py-20">
+      <div className="flex flex-col tablet:w-[550px] w-full">
+
+        <div className="flex items-center mb-6 tablet:mb-10 desktop:mb-10">
+          <Button
+            variant="text"
+            onClick={resetForm}
+            className="min-w-[60px] tablet:min-w-[120px] desktop:min-w-[120px]">
+            <Icon name="arrowLeft" width={24} height={24} />
+          </Button>
+          <Text className="text-body-32 tablet:text-body-40 desktop:text-body-40 font-semibold flex-1">
+            Create New Collection - {type}
+          </Text>
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-4 p-4">
+            {/* Upload file */}
+            <div>
+              <Text className="text-base font-semibold mb-1">Collection image</Text>
+              <Controller
+                name="avatar"
+                control={control}
+                rules={formRules.image}
+                render={({ field: { value } }) => (
+                  <ImageUploader
+                    value={value}
+                    onInput={handleUploadImage}
+                    loading={uploading}
+                    error={!!errors.avatar}
+                    maxSize={20}
+                    accept=".png,.jpeg, .png, .gif, .webp"
+                  />
+                )}
+              />
+            </div>
+            {/* Name */}
+            <div>
+              <Text className="text-base font-semibold mb-1">Display name</Text>
+              <Input register={register('name', formRules.name)} error={!!errors.name} />
+            </div>
+            {/* Symbol */}
+            <div>
+              <Text className="text-base font-semibold mb-1">Symbol</Text>
+              <Input register={register('symbol', formRules.symbol)} error={!!errors.symbol} />
+            </div>
+            <div>
+              <Text className="text-base font-semibold mb-1">Short URL</Text>
+              <Input
+                error={!!errors.shortUrl}
+                register={register('shortUrl', formRules.shortUrl)}
+              />
+            </div>
+            {/* Description */}
+            <div>
+              <Text className="text-base font-semibold mb-1">Description (optional)</Text>
+              <Textarea
+                className="h-[160px] resize-none"
+                register={register('description', formRules.description)}
+                error={!!errors.description}
+              />
+            </div>
+
+            <FormValidationMessages errors={errors} />
+
+            {/* Button finish */}
+            <ConnectWalletButton>
+              <Button
+                loading={loading}
+                loadingText="Creating collection ..."
+                disabled={validating || uploading}
+                type="submit"
+                className="w-full tablet:w-auto desktop:w-auto">
+                Create collection
+              </Button>
+            </ConnectWalletButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
---
+++ app/(app)/create/nft/[type]/page.tsx
@@ -0,0 +1,420 @@
+'use client'
+
+import Button from "@/components/Button";
+import Input from "@/components/Form/Input";
+import Textarea from "@/components/Form/Textarea";
+import Text from "@/components/Text";
+import { useEffect, useMemo, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import Icon from '@/components/Icon'
+import { AssetType, Trait } from '@/types'
+import { classNames } from '@/utils/string'
+import useAuthStore from '@/store/auth/store'
+import useSWR from 'swr'
+import { useMarketplaceApi } from '@/hooks/useMarketplaceApi'
+import { Accordion, Tooltip } from 'flowbite-react'
+import Link from 'next/link'
+import { Address } from 'wagmi'
+import { toast } from 'react-toastify'
+import ConnectWalletButton from '@/components/Button/ConnectWalletButton'
+import FormValidationMessages from '@/components/Form/ValidationMessages'
+import { useCreateNFT } from '@/hooks/useNFT'
+import ImageUploader from "@/components/Form/ImageUploader";
+import { ALLOWED_FILE_TYPES, ALLOWED_IMAGE_TYPES } from '@/config/constants'
+import PlusCircleIcon from "@/components/Icon/PlusCircle";
+import { redirect, useParams, useRouter } from "next/navigation";
+
+interface NFTFormState {
+  media: Blob[],
+  name: string,
+  description: string,
+  collection: Address,
+  royalties: number
+  amount: number
+  traits: Trait[]
+}
+
+export default function CreateNftPage() {
+  const type = useParams().type.toString().toUpperCase() as AssetType
+  const router = useRouter()
+  const [loading, setLoading] = useState(false)
+  const [validating, setValidating] = useState(false)
+  const [uploading, setUploading] = useState(false)
+  const api = useMarketplaceApi()
+  const { onCreateNFT } = useCreateNFT(type || 'ERC721')
+  const userId = useAuthStore(state => state.credentials?.userId)
+  const { data, error, isLoading } = useSWR(
+    !!userId ? userId : null,
+    (userId) => api.fetchCollectionsByUser(userId as string, { page: 1, limit: 1000 }, true),
+    { refreshInterval: 3600 * 1000 }
+  )
+  const {
+    handleSubmit,
+    register,
+    control,
+    setError,
+    clearErrors,
+    getValues,
+    reset,
+    setValue,
+    watch,
+    formState: { errors }
+  } = useForm<NFTFormState>({
+    defaultValues: { traits: [{ trait_type: '', value: '' }] }
+  })
+
+  const media = watch('media')
+  const isNonImageNFT = useMemo(() => {
+    if (!media || !media?.length) return false
+    const fileType = media[0].type.split('/')[0]
+    if (!fileType) return false
+    return fileType !== 'image'
+  }, [media])
+
+  const formRules = {
+    media: {
+      validate: {
+        required: (v: Blob[]) => v.length > 0 || 'Collection image is required!',
+        audio: (values: Blob[]) => {
+          const firstFileType = values[0].type.split('/')[0]
+          if (firstFileType && firstFileType !== 'audio') return true
+          return !!values[1] || 'Cover photo is required'
+        }
+      },
+    },
+    name: {
+      required: 'Collection name is required!'
+    },
+    collection: {
+      required: 'Please choose a collection'
+    },
+    description: {
+      maxLength: { value: 256, message: 'Description cannot exceed 256 characters' }
+    },
+    royalties: {
+      required: 'Royalties is required',
+      min: { value: 1, message: 'Royalties should be within range of 1% - 50%' },
+      max: { value: 50, message: 'Royalties should be within range of 1% - 50%' }
+    },
+    amount: {
+      validate: (value: number) => {
+        if (type === 'ERC721') return true
+        return value > 0 || 'Quantity must be greater than 1'
+      }
+    }
+  }
+
+  const collectionOptions = useMemo(() => {
+    const collections = data?.data?.map(c => ({
+      label: c.name ?? c.id, value: c.address, type: c.type
+    })) || []
+    return collections.filter(c => c.type === type)
+  }, [data, type])
+
+  const resetForm = () => {
+    reset()
+    router.push('/create/nft')
+  }
+
+  const handleSelectMedia = (file?: Blob) => {
+    if (!file) {
+      setValue('media', [])
+    } else {
+      setValue('media', [file])
+    }
+  }
+
+  const handleSelectCoverImage = (file?: Blob) => {
+    if (!file) {
+      setValue('media', [media[0]])
+    } else {
+      setValue('media', [media[0], file])
+    }
+  }
+
+  const handleTraitInput = (index: number, key: keyof Trait, value: any) => {
+    const _traits = getValues('traits')
+    if (!_traits) return
+    const _trait = { ..._traits[index] }
+    if (!_trait) return
+    _trait[key] = value
+    _traits[index] = _trait
+    setValue('traits', _traits)
+
+    const lastTrait = _traits[_traits.length - 1]
+    if (!lastTrait) return
+    if (!lastTrait.trait_type || !lastTrait.value) return
+    _traits.push({ trait_type: '', value: '' }) // Add more trait when inputting
+    setValue('traits', _traits as Trait[])
+  }
+
+  const onSubmit = async ({ media, traits, ...rest }: NFTFormState) => {
+    if (!type) return
+
+    setLoading(true)
+    const _traits = !!traits ? traits.filter(trait => !!trait.trait_type && trait.value) : []
+    const createNFTToast = toast.loading('Uploading Media...', { type: 'info' })
+
+    try {
+      const { fileHashes } = await api.uploadFile(media)
+      toast.update(createNFTToast, { render: 'Sending transaction' })
+
+      const [media1, media2] = fileHashes
+      const params = {
+        ...rest,
+        traits: _traits,
+        image: isNonImageNFT ? media2 : media1,
+        animation_url: isNonImageNFT ? media1 : undefined
+      }
+
+      await onCreateNFT(params)
+
+      toast.update(createNFTToast, {
+        render: 'Item created successfully!',
+        type: 'success',
+        isLoading: false,
+        autoClose: 1000
+      })
+      resetForm()
+      router.push('/explore/items')
+    } catch (e: any) {
+      console.error(e)
+      toast.update(createNFTToast, {
+        render: error => `Error report: ${e.message}`,
+        type: 'error',
+        isLoading: false,
+        autoClose: 1000
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const handleValidateInput = async (name: string, value: Record<string, any>) => {
+    try {
+      setValidating(true)
+      if (name === 'name' && !!value.name) {
+        if (!getValues('collection')) {
+          return
+        }
+        const existed = await api.validateInput({
+          key: 'nftName',
+          value: value.name,
+          collectionId: getValues('collection')
+        })
+        if (existed) setError('name', { type: 'custom', message: 'NFT name already existed' })
+        else clearErrors('name')
+      }
+    } finally {
+      setValidating(false)
+    }
+  }
+
+  useEffect(() => {
+    const subscription = watch(async (value, { name, type }) => {
+      if (!name) return
+      handleValidateInput(name, value)
+    })
+    return () => subscription.unsubscribe()
+  }, [watch])
+
+  useEffect(() => {
+    if (type !== 'ERC1155' && type !== 'ERC721') {
+      return redirect('/create/nft')
+    }
+  }, [type]);
+
+  return (
+    <div className="w-full flex justify-center py-10 tablet:py-20 desktop:py-20">
+      <div className="flex flex-col tablet:w-[550px] w-full">
+        <div className="flex items-center mb-6 tablet:mb-10 desktop:mb-10">
+          <Button
+            variant="text"
+            onClick={resetForm}
+            className="min-w-[60px] tablet:min-w-[120px] desktop:min-w-[120px]">
+            <Icon name="arrowLeft" width={24} height={24} />
+          </Button>
+          <Text className="text-body-32 tablet:text-body-40 desktop:text-body-40 font-semibold">
+            Create New NFT - {type}
+          </Text>
+        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex flex-col gap-4 p-4">
+            {/* Upload file */}
+            <div>
+              <Text className="text-body-16 font-semibold mb-1">Upload file</Text>
+              <Controller
+                name="media"
+                control={control}
+                rules={formRules.media}
+                render={({ field: { value } }) => (
+                  <ImageUploader
+                    maxSize={100}
+                    loading={uploading}
+                    error={!!errors.media}
+                    accept={ALLOWED_FILE_TYPES}
+                    onInput={handleSelectMedia} />
+                )}
+              />
+            </div>
+            {
+              isNonImageNFT && (
+                <div>
+                  <Text className="text-body-16 font-semibold mb-1">Upload Cover Image</Text>
+                  <Controller
+                    name="media"
+                    control={control}
+                    render={({ field: { value } }) => (
+                      <ImageUploader
+                        loading={uploading}
+                        error={!!errors.media}
+                        accept={ALLOWED_IMAGE_TYPES}
+                        maxSize={20}
+                        onInput={handleSelectCoverImage} />
+                    )}
+                  />
+                </div>
+              )
+            }
+
+            {/* Choose collection */}
+            <div>
+              <Text className="text-body-16 font-semibold mb-1">Choose collection</Text>
+              <Controller
+                name="collection"
+                control={control}
+                rules={formRules.collection}
+                render={({ field: { onChange, value } }) => (
+                  <div className="flex items-center gap-3 w-full max-h-56 overflow-y-auto flex-wrap">
+                    {
+                      type === 'ERC721' || type === 'ERC1155' ?
+                        <Link href={`/create/collection`}>
+                          <div className={classNames(
+                            'w-36 overflow-ellipsis flex flex-col justify-center items-center gap-2 cursor-pointer rounded-2xl p-5 text-center border-2 text-primary bg-white',
+                            'hover:border-primary hover:bg-white hover:text-primary transition-all',
+                          )}>
+                            <PlusCircleIcon width={24} height={24} />
+                            <span className="font-bold">Create <span className="text-tertiary font-normal"> {type === 'ERC721' ? 'ERC721' : 'ERC1155'}</span></span>
+                          </div>
+                        </Link>
+                        : ''
+                    }
+                    {
+                      collectionOptions.length ? collectionOptions.map(c => (
+                        <div
+                          key={c.value}
+                          onClick={() => onChange(c.value)}
+                          className={classNames(
+                            'w-36 overflow-ellipsis flex flex-col justify-center items-center gap-2 cursor-pointer rounded-2xl p-8 text-center',
+                            'hover:border-primary hover:bg-white hover:text-primary border-2 transition-all',
+                            c.value === value ? 'border-primary bg-white text-primary' : 'text-tertiary bg-surface-soft'
+                          )}>
+                          <Tooltip content={c.label} placement="top">
+                            <Text className="text-body-18 font-bold text-primary text-ellipsis w-[7rem] break-all whitespace-nowrap overflow-hidden">{c.label}</Text>
+                          </Tooltip>
+                          <Text className="text-body-12 text-secondary text-ellipsis w-[7rem] break-all whitespace-nowrap overflow-hidden">{c.type}</Text>
+
+                        </div>
+                      )) : (
+                        <div className="flex justify-center items-center w-full h-40 rounded-2xl border border-dashed border-disabled">
+                          <Link className="text-center" href={`/create/collection`}>Create Collection</Link>
+                        </div>
+                      )
+                    }
+                  </div>
+                )}
+              />
+            </div>
+            {/* Name */}
+            <div>
+              <Text className="text-body-16 font-semibold mb-1">Display name</Text>
+              <Input
+                error={!!errors.name}
+                register={register('name', formRules.name)}
+              />
+            </div>
+            {/* Description */}
+            <div>
+              <Text className="text-body-16 font-semibold mb-1">Description</Text>
+              <Textarea
+                className="h-[160px] resize-none"
+                error={!!errors.description}
+                register={register('description', formRules.description)}
+              />
+            </div>
+            {/* Royalties */}
+            <div>
+              <Text className="text-body-16 font-semibold mb-1">Royalties</Text>
+              <Input
+                type="number"
+                error={!!errors.royalties}
+                register={register('royalties', formRules.royalties)}
+                appendIcon={(
+                  <Text className="text-secondary">%</Text>
+                )}
+              />
+            </div>
+
+            {type === 'ERC1155' && (
+              <div>
+                <Text className="text-body-16 font-semibold mb-1">Number of copies</Text>
+                <Input
+                  error={!!errors.amount}
+                  register={register('amount', formRules.amount)}
+                />
+              </div>
+            )}
+
+            <Accordion collapseAll>
+              <Accordion.Panel>
+                <Accordion.Title>Advanced settings</Accordion.Title>
+                <Accordion.Content>
+                  <Text className="text-primary font-semibold mb-4" variant="body-16">
+                    Properties <span className="text-secondary">(Optional)</span>
+                  </Text>
+
+                  <div className="w-full flex flex-col gap-4">
+                    <Controller
+                      name="traits"
+                      control={control}
+                      render={({ field: { onChange, value } }) => {
+                        return (
+                          <>{Array.isArray(value) && value.map((trait, index) => (
+                            <div key={index} className="w-full flex items-center gap-7">
+                              <Input
+                                value={value[index].trait_type}
+                                containerClass="flex-1"
+                                placeholder="e.g. Size"
+                                onChange={event => handleTraitInput(index, 'trait_type', event.target.value)} />
+                              <Input
+                                value={value[index].value}
+                                containerClass="flex-1"
+                                placeholder="e.g. M"
+                                onChange={event => handleTraitInput(index, 'value', event.target.value)} />
+                            </div>
+                          ))}</>
+                        )
+                      }} />
+                  </div>
+                </Accordion.Content>
+              </Accordion.Panel>
+            </Accordion>
+
+            <FormValidationMessages errors={errors} />
+
+            <ConnectWalletButton>
+              <Button
+                loading={loading}
+                loadingText="Creating NFT ..."
+                disabled={uploading || validating}
+                type="submit"
+                className="w-full tablet:w-auto desktop:w-auto">
+                Create Item
+              </Button>
+            </ConnectWalletButton>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
