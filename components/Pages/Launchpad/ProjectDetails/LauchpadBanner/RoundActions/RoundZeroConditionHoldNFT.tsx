9c7277b43ca57532d2bc56dc58bce5bf2e8f7793
Dat <eminemempire3@gmail.com>
2024-01-27 15:39:27 +0700

    refactor launchpad round-related

---
+++ components/Pages/Launchpad/ProjectDetails/LauchpadBanner/RoundActions/RoundZeroConditionHoldNFT.tsx
@@ -0,0 +1,67 @@
+import Icon from '@/components/Icon';
+import { MARKETPLACE_URL, ZERO_COLLECTION } from '@/config/constants';
+import { classNames } from '@/utils/string';
+import Link from 'next/link';
+
+interface Props {
+  balanceNFT: string;
+  isHolder: boolean;
+}
+
+export default function RoundZeroConditionHoldNFT({
+  isHolder,
+  balanceNFT,
+}: Props) {
+  return (
+    <div
+      className={classNames(
+        'desktop:w-1/2 order-1 border-2 rounded-2xl transition-all p-4',
+        isHolder
+          ? ' border-success'
+          : 'border-dashed border-gray-500/70 hover:border-gray-500 hover:border-solid'
+      )}
+    >
+      <p className='font-semibold text-center text-body-18'>Zero Collection</p>
+
+      <div className='flex items-center justify-between text-body-12'>
+        <p className='text-secondary font-medium'>Condition:</p>
+        <p className='text-primary font-semibold'>Own Zero Collection</p>
+      </div>
+
+      <div className='flex items-center justify-between text-body-12'>
+        <p className='text-secondary font-medium'>Currently own:</p>
+        <p className='text-primary font-semibold'>{balanceNFT} items</p>
+      </div>
+
+      <div className='flex items-center justify-between text-body-12'>
+        <p className='text-secondary font-medium'>Status:</p>
+        <div className='text-primary font-semibold'>
+          {isHolder ? (
+            <div className='flex items-center gap-1'>
+              <Icon name='verified' />
+              <span className='text-success'>Qualified</span> |{' '}
+              <Link
+                href={MARKETPLACE_URL + `/collection/${ZERO_COLLECTION}`}
+                className='hover: underline'
+                target='_blank'
+              >
+                Get more
+              </Link>
+            </div>
+          ) : (
+            <div className='flex items-center gap-1'>
+              <span className='text-error'>Not Qualified</span> |{' '}
+              <Link
+                href={MARKETPLACE_URL + `/collection/${ZERO_COLLECTION}`}
+                className='hover: underline'
+                target='_blank'
+              >
+                Get now
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
