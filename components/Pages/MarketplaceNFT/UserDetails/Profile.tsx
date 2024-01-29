d839d1c9f5ad7f316cb9fc0ed6f505b00222346c
damthang12 <thangdq@uniultra.xyz>
2024-01-29 12:56:48 +0700

    handle follow button

--- components/Pages/MarketplaceNFT/UserDetails/Profile.tsx
+++ components/Pages/MarketplaceNFT/UserDetails/Profile.tsx
@@ -35,7 +35,7 @@ export default function Profile({
           <div className="absolute border-white rounded-2xl desktop:pl-[80px] tablet:pl-[80px] pl-4"
                style={{ bottom: '0', transform: 'translateY(50%)' }}>
             <Image
-                src={avatar || getUserAvatarImage()}
+                src={avatar || getUserAvatarImage(username)}
                 alt="user-detail-bg"
                 width={120} height={120}
                 className="rounded-2xl w-[80px] h-[80px] tablet:w-[120px] desktop:w-[120px] tablet:h-[120px] desktop:h-[120px]"/>
