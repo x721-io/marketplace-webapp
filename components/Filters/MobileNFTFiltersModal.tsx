313d51313f118a338ed99d8a386ad9fee1272c21
Kim Chi Nguyen <chinpk@uniultra.xyz>
2024-03-07 15:18:51 +0700

    [FIX] Fix UI filter

--- components/Filters/MobileNFTFiltersModal.tsx
+++ components/Filters/MobileNFTFiltersModal.tsx
@@ -144,7 +144,7 @@ export default function MobileNFTFiltersModal({
               />
               <Select
                 options={tokenOptions}
-                containerClass="tablet:w-2/3 w-3/4"
+                containerClass="tablet:w-2/3 w-full"
                 scale="sm"
                 value={localFilters.quoteToken}
                 onChange={(e) =>
