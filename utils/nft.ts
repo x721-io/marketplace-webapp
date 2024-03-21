df613120f245208779b7024285a6f039d903ed19
Kim Chi Nguyen <chinpk@uniultra.xyz>
2024-03-21 16:44:16 +0700

    [FIX] ipfsPrefix

--- utils/nft.ts
+++ utils/nft.ts
@@ -9,12 +9,12 @@ export const convertImageUrl = (url?: string): string => {
   const baseURL = BASE_API_URL + "/common/ipfs-serve?ipfsPath=";
   if (!url) return "";
 
-  const ipfsPrefix = ["https://ipfs.io/ipfs/", "testnet-api.memetaverse.club"]; 
+  const ipfsPrefix = ["https://ipfs.io/ipfs/", "testnet-api.memetaverse.club", "u2u-images.s3.ap-southeast-1.amazonaws.com"]; 
 
   if (url.startsWith(ipfsPrefix[0])) { 
     const ipfsPath = url.replace(ipfsPrefix[0], "ipfs://ipfs/");
     return baseURL + encodeURIComponent(ipfsPath);
-  }  else if (url.includes(baseURL) || (url.includes(ipfsPrefix[1]))) {
+  }  else if (url.includes(baseURL) || (ipfsPrefix.some(prefix => url.includes(prefix)))) {
     return url;
   } else {
     return baseURL + encodeURIComponent(url);
