c821bb141e38dc7e494bfa6413805d18fae4e862
kimkimchichi <chinpk@uniultra.xyz>
2024-05-03 21:27:13 +0700

    Update token.ts
    [FIX] Fix crash explore nft page

--- utils/token.ts
+++ utils/token.ts
@@ -2,7 +2,7 @@ import { Address } from "wagmi";
 import { tokens } from "@/config/tokens";
 
 export const findTokenByAddress = (address?: Address) => {
-  if (address?.toLowerCase() === tokens.wu2u.address.toLowerCase()) {
+  if (address?.toLowerCase() === tokens.wu2u.address?.toLowerCase()) {
     return {
       ...tokens.wu2u,
       name: "Unicorn Ultra Token",
