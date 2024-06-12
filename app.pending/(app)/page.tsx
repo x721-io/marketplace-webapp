5c32822434d770b5c560f24ae879652eac0bc163
vietthai244 <thai.toviet@gmail.com>
2024-06-12 10:27:14 +0700

    [UPDATE] prepare for mainnet prerelease

---
+++ app.pending/(app)/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import HomePageBanner from "@/components/Pages/MarketplaceNFT/HomePage";
+
+export default function Home() {
+  return (
+    <div className="">
+      <HomePageBanner />
+    </div>
+  );
+}
---
+++ app.pending/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import "tailwindcss/tailwind.css";
+import Providers from "@/components/Providers";
+import ErrorBoundary from "@/components/ErrorBoundary";
+import { ToastContainer } from "react-toastify";
+import React, { Suspense } from "react";
+import LoadingPage from "./loading";
+
+export const metadata: Metadata = {
+  title: "U2U NFT Marketplace",
+  description: "U2U NFT Marketplace",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ErrorBoundary>
+          <Providers>
+            <Suspense fallback={<LoadingPage />}>{children}</Suspense>
+            <ToastContainer autoClose={5000} />
+          </Providers>
+        </ErrorBoundary>
+      </body>
+    </html>
+  );
+}
