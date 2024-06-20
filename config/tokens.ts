9dcddd9ab988db1645e3b3b171cc4c15db51fbc3
damthang12 <damthangjp1201@gmail.com>
2024-06-20 17:35:09 +0700

    [UPDATE] update config tokens

--- config/tokens.ts
+++ config/tokens.ts
@@ -16,20 +16,20 @@ export const tokens: Record<string, Token> = {
     address: process.env.NEXT_PUBLIC_WU2U_CONTRACT as Address,
     logo: "https://play-lh.googleusercontent.com/NLVnM9o_BuPceMiPEiTCiMsD0KeCjzZqPc_Cj6iMPyzsHXReGkssZihl2vf6NL7qXpI",
   },
-  weth: {
-    name: "WETH",
-    symbol: "WETH",
-    decimal: 18,
-    address: process.env.NEXT_PUBLIC_WETH_CONTRACT as Address,
-    logo: "https://ug-assets-dev.s3.ap-southeast-1.amazonaws.com/0d38aae1-e5d0-4b10-9e67-b85b13ce0891-wETH.jpg",
-  },
-  wbtc: {
-    name: "WBTC",
-    symbol: "WBTC",
-    decimal: 18,
-    address: process.env.NEXT_PUBLIC_WBTC_CONTRACT as Address,
-    logo: "https://ug-assets-dev.s3.ap-southeast-1.amazonaws.com/cc3ff3dd-1d7a-4254-975b-62243326cd6d-wBTC.jpg",
-  }
+  // weth: {
+  //   name: "WETH",
+  //   symbol: "WETH",
+  //   decimal: 18,
+  //   address: process.env.NEXT_PUBLIC_WETH_CONTRACT as Address,
+  //   logo: "https://ug-assets-dev.s3.ap-southeast-1.amazonaws.com/0d38aae1-e5d0-4b10-9e67-b85b13ce0891-wETH.jpg",
+  // },
+  // wbtc: {
+  //   name: "WBTC",
+  //   symbol: "WBTC",
+  //   decimal: 18,
+  //   address: process.env.NEXT_PUBLIC_WBTC_CONTRACT as Address,
+  //   logo: "https://ug-assets-dev.s3.ap-southeast-1.amazonaws.com/cc3ff3dd-1d7a-4254-975b-62243326cd6d-wBTC.jpg",
+  // }
 };
 
 export const tokenOptions = Object.values(tokens).map((token) => {
