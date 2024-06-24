1e29593818f25a3cd7e6fc525b795ff7413569dd
vietthai244 <thai.toviet@gmail.com>
2024-06-24 21:46:16 +0700

    [UPDATE] Maintain

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ToastContainer } from "react-toastify";
 import React, { Suspense } from "react";
 import LoadingPage from "./loading";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Text from '@/components/Text'
 
 export const metadata: Metadata = {
   title: "X721 Marketplace",
@@ -19,15 +20,30 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const underMaintenance = true;
+
+
   return (
     <html lang="en">
       <body>
-        <ErrorBoundary>
-          <Providers>
-            <Suspense fallback={<LoadingPage />}>{children}</Suspense>
-            <ToastContainer autoClose={5000} />
-          </Providers>
-        </ErrorBoundary>
+        {underMaintenance ? (
+          <div className="w-screen h-screen flex flex-col justify-center items-center">
+            <Text className="text-primary text-center font-semibold text-heading-sm">
+              Service under Maintenance.
+            </Text>
+            <br/>
+            <Text className="text-secondary text-center text-body-24">
+              We will comeback shortly.
+            </Text>
+          </div>
+          ) : (
+          <ErrorBoundary>
+            <Providers>
+              <Suspense fallback={<LoadingPage />}>{children}</Suspense>
+              <ToastContainer autoClose={5000} />
+            </Providers>
+          </ErrorBoundary>
+        )}
         <GoogleAnalytics gaId="G-4BKP4Z6M3K" />
       </body>
     </html>
