192bad0daba84ea2b561f2d2b0f509e59d52a593
damthang12 <damthangjp1201@gmail.com>
2024-08-21 11:41:08 +0700

    [UPDATE] handle u2uid and id for approve token single

--- hooks/useMarketApproveNFT.ts
+++ hooks/useMarketApproveNFT.ts
@@ -70,7 +70,7 @@ export const useMarketApproveNFT = (nft: NFT) => {
       address: nft.collection.address,
       abi: contracts.erc721Base.abi,
       functionName: "approve",
-      args: [contracts.erc721Market.address, BigInt(nft.u2uId)],
+      args: [contracts.erc721Market.address, BigInt(nft.u2uId || nft.id)],
       value: BigInt(0) as any,
     });
     return waitForTransaction({ hash });
