e391689a4cbf97e7c5680297884f5d504ed3db2c
vulh <vulh@uniultra.xyz>
2024-09-06 16:24:19 +0700

    [chore]: test build

--- components/Pages/Launchpad/ProjectDetails/MintSchedule.tsx
+++ components/Pages/Launchpad/ProjectDetails/MintSchedule.tsx
@@ -77,111 +77,116 @@ const RoundSchedule = ({
   };
 
   return (
-    <div className="flex items-stretch gap-2 w-full" {...rest}>
-      <div className="flex flex-col items-center">
-        <div
-          className={classNames(
-            "w-8 h-8 rounded-full flex items-center justify-center",
-            status === "ENDED" && "bg-success/50",
-            status === "MINTING" && "bg-success/50",
-            status === "UPCOMING" &&
-              (isActive ? "bg-warning/50" : "bg-surface-medium")
-          )}
-        >
+    <>
+      <div className="flex items-stretch gap-2 w-full" {...rest}>
+        <div className="flex flex-col items-center">
           <div
             className={classNames(
-              "w-6 h-6 rounded-full flex items-center justify-center",
-              status === "ENDED" && "bg-success",
-              status === "MINTING" && "bg-white",
+              "w-8 h-8 rounded-full flex items-center justify-center",
+              status === "ENDED" && "bg-success/50",
+              status === "MINTING" && "bg-success/50",
               status === "UPCOMING" &&
-                (isActive ? "bg-white" : "bg-surface-soft")
+                (isActive ? "bg-warning/50" : "bg-surface-medium")
             )}
           >
-            {isCompleted && (
-              <Icon color="white" name="check" width={16} height={16} />
-            )}
+            <div
+              className={classNames(
+                "w-6 h-6 rounded-full flex items-center justify-center",
+                status === "ENDED" && "bg-success",
+                status === "MINTING" && "bg-white",
+                status === "UPCOMING" &&
+                  (isActive ? "bg-white" : "bg-surface-soft")
+              )}
+            >
+              {isCompleted && (
+                <Icon color="white" name="check" width={16} height={16} />
+              )}
+            </div>
           </div>
+          {showSeparator && (
+            <div
+              className={classNames(
+                "flex-1 min-h-[20px] w-[2px]",
+                isCompleted ? "bg-success" : "bg-surface-soft"
+              )}
+            />
+          )}
         </div>
-        {showSeparator && (
-          <div
-            className={classNames(
-              "flex-1 min-h-[20px] w-[2px]",
-              isCompleted ? "bg-success" : "bg-surface-soft"
-            )}
-          />
-        )}
+
+        <Collapsible
+          // defaultOpen={isActive || claimable}
+          className="flex-1 !px-0 !py-1"
+          header={<p className="text-body-16 font-medium">{round?.name}</p>}
+        >
+          <div className="rounded-2xl bg-surface-soft p-4 my-4 flex flex-col gap-2">
+            <div className="flex items-center gap-1">
+              <p className="text-body-16 font-medium">Minting</p>
+              {status === "UPCOMING" && (
+                <p className="text-body-16 text-warning">Upcoming</p>
+              )}
+              {status === "MINTING" && (
+                <p className="text-body-16 text-success">Live</p>
+              )}
+              {status === "ENDED" && (
+                <p className="text-body-16 text-error">Ended</p>
+              )}
+            </div>
+
+            <p className="text-body-12 text-secondary">
+              Start:{" "}
+              {format(new Date(round?.start || 0), "yyyy/M/dd - hh:mm a")}
+            </p>
+            <p className="text-body-12 text-secondary">
+              End: {format(new Date(round?.end || 0), "yyyy/M/dd - hh:mm a")}
+            </p>
+
+            <div className="w-full h-[1px] bg-gray-200" />
+
+            <div className="flex items-center gap-1">
+              <p className="text-body-12 text-secondary">Price:</p>
+              <Icon name="u2u-logo" width={16} height={16} />
+              <p className="text-body-12 font-medium">
+                {formatDisplayedNumber(formatEther(round?.price || 0))} U2U
+              </p>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <p className="text-body-12 text-secondary">Total mintable:</p>
+              <p className="text-body-12 font-medium">
+                {round?.totalNftt === 0
+                  ? "Open edition"
+                  : formatDisplayedNumber(round?.totalNftt)}
+              </p>
+            </div>
+
+            <div className="flex items-center gap-1">
+              <p className="text-body-12 text-secondary">Max mint:</p>
+              <p className="text-body-12 font-medium">
+                {round?.maxPerWallet === 0
+                  ? "Open edition"
+                  : round?.maxPerWallet}
+              </p>
+            </div>
+
+            <p className="text-body-12 text-secondary">
+              Claimable at:{" "}
+              {format(
+                new Date(round?.claimableStart || 0),
+                "yyyy/M/dd - hh:mm a"
+              )}
+            </p>
+
+            {claimable &&
+              Number(claimableAmount) > 0 &&
+              Number(startClaim) !== 0 && (
+                <Button scale="sm" onClick={handleClaimNFT} loading={loading}>
+                  Claim now
+                </Button>
+              )}
+          </div>
+        </Collapsible>
       </div>
-
-      <Collapsible
-        // defaultOpen={isActive || claimable}
-        className="flex-1 !px-0 !py-1"
-        header={<p className="text-body-16 font-medium">{round?.name}</p>}
-      >
-        <div className="rounded-2xl bg-surface-soft p-4 my-4 flex flex-col gap-2">
-          <div className="flex items-center gap-1">
-            <p className="text-body-16 font-medium">Minting</p>
-            {status === "UPCOMING" && (
-              <p className="text-body-16 text-warning">Upcoming</p>
-            )}
-            {status === "MINTING" && (
-              <p className="text-body-16 text-success">Live</p>
-            )}
-            {status === "ENDED" && (
-              <p className="text-body-16 text-error">Ended</p>
-            )}
-          </div>
-
-          <p className="text-body-12 text-secondary">
-            Start: {format(new Date(round?.start || 0), "yyyy/M/dd - hh:mm a")}
-          </p>
-          <p className="text-body-12 text-secondary">
-            End: {format(new Date(round?.end || 0), "yyyy/M/dd - hh:mm a")}
-          </p>
-
-          <div className="w-full h-[1px] bg-gray-200" />
-
-          <div className="flex items-center gap-1">
-            <p className="text-body-12 text-secondary">Price:</p>
-            <Icon name="u2u-logo" width={16} height={16} />
-            <p className="text-body-12 font-medium">
-              {formatDisplayedNumber(formatEther(round?.price || 0))} U2U
-            </p>
-          </div>
-
-          <div className="flex items-center gap-1">
-            <p className="text-body-12 text-secondary">Total mintable:</p>
-            <p className="text-body-12 font-medium">
-              {round?.totalNftt === 0
-                ? "Open edition"
-                : formatDisplayedNumber(round?.totalNftt)}
-            </p>
-          </div>
-
-          <div className="flex items-center gap-1">
-            <p className="text-body-12 text-secondary">Max mint:</p>
-            <p className="text-body-12 font-medium">
-              {round?.maxPerWallet === 0 ? "Open edition" : round?.maxPerWallet}
-            </p>
-          </div>
-
-          <p className="text-body-12 text-secondary">
-            Claimable at:{" "}
-            {format(
-              new Date(round?.claimableStart || 0),
-              "yyyy/M/dd - hh:mm a"
-            )}
-          </p>
-
-          {claimable &&
-            Number(claimableAmount) > 0 &&
-            Number(startClaim) !== 0 && (
-              <Button scale="sm" onClick={handleClaimNFT} loading={loading}>
-                Claim now
-              </Button>
-            )}
-        </div>
-      </Collapsible>
-    </div>
+    </>
   );
 };
 
