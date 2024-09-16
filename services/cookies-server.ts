52e7dee160a60931941d03360928c4b5df4872e7
vulh <vulh@uniultra.xyz>
2024-09-16 16:07:16 +0700

    [FIX]:  reverse code

--- services/cookies-server.ts
+++ services/cookies-server.ts
@@ -8,30 +8,15 @@ export const setAuthCookies = ({
   refreshTokenExpire,
   userId,
 }: APIResponse.Connect) => {
-  console.log("ACCESS_TOKEN:");
-  console.log(accessToken);
-  console.log(accessTokenExpire);
   cookies().set("accessToken", accessToken, {
     expires: accessTokenExpire,
-    sameSite: "none",
-    path: "/",
   });
   cookies().set("refreshToken", refreshToken, {
     expires: refreshTokenExpire,
-    sameSite: "none",
-    path: "/",
   });
-  console.log("REFRESH_TOKEN:");
-  console.log(refreshToken);
-  console.log(refreshTokenExpire);
   cookies().set("userId", userId, {
     expires: accessTokenExpire,
-    sameSite: "none",
-    path: "/",
   });
-  console.log("USER_ID:");
-  console.log(userId);
-  console.log(accessTokenExpire);
 };
 
 export const getAuthCookies = () => {
