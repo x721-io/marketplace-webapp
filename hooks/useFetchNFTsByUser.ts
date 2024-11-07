e608f3c044a8b48927b8123df736406be7271196
winD <105033231+damthang12@users.noreply.github.com>
2024-11-07 18:24:17 +0700

    Fix bug (#25)
    * [UPDATE] fix reset filter on profile
    
    * [UPDATE] update rule upload

--- hooks/useFetchNFTsByUser.ts
+++ hooks/useFetchNFTsByUser.ts
@@ -1,5 +1,5 @@
 import { useFilterByUser } from "@/store/filters/byUser/store";
-import { useMemo } from "react";
+import { useCallback, useMemo } from "react";
 import { APIParams } from "@/services/api/types";
 import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
 import { Address } from "abitype";
@@ -10,21 +10,39 @@ export const useFetchNFTsByUser = (
   mode: "created" | "owned" | "onSale"
 ) => {
   const filterStore = useFilterByUser();
-  const { showFilters, filters, toggleFilter, resetFilters, updateFilters } =
-    useMemo(() => {
-      console.log(mode);
-      return {
-        showFilters: filterStore[wallet]?.[mode].showFilters,
-        filters: filterStore[wallet]?.[mode].filters || {},
-        toggleFilter: (bool?: boolean) =>
-          filterStore.toggleFilter(mode, wallet, bool),
-        setFilters: (filters: APIParams.FetchNFTs) =>
-          filterStore.setFilters(mode, wallet, filters),
-        updateFilters: (filters: Partial<APIParams.FetchNFTs>) =>
-          filterStore.updateFilters(mode, wallet, filters),
-        resetFilters: () => filterStore.resetFilters(mode, wallet),
-      };
-    }, [filterStore, mode, wallet]);
+  // const { showFilters, filters, toggleFilter, resetFilters, updateFilters } =
+  //   useMemo(() => {
+  //     console.log(mode);
+  //     return {
+  //       showFilters: filterStore[wallet]?.[mode].showFilters,
+  //       filters: filterStore[wallet]?.[mode].filters || {},
+  //       toggleFilter: (bool?: boolean) =>
+  //         filterStore.toggleFilter(mode, wallet, bool),
+  //       setFilters: (filters: APIParams.FetchNFTs) =>
+  //         filterStore.setFilters(mode, wallet, filters),
+  //       updateFilters: (filters: Partial<APIParams.FetchNFTs>) =>
+  //         filterStore.updateFilters(mode, wallet, filters),
+  //       resetFilters: () => filterStore.resetFilters(mode, wallet),
+  //     };
+  //   }, [filterStore, mode, wallet]);
+
+  const showFilters = filterStore[wallet]?.[mode]?.showFilters || false;
+  const filters = filterStore[wallet]?.[mode]?.filters || {};
+
+  const toggleFilter = useCallback(
+    (bool?: boolean) => filterStore.toggleFilter(mode, wallet, bool),
+    [filterStore, mode, wallet]
+  );
+
+  const updateFilters = useCallback(
+    (newFilters: Partial<APIParams.FetchNFTs>) =>
+      filterStore.updateFilters(mode, wallet, newFilters),
+    [filterStore, mode, wallet]
+  );
+
+  const resetFilters = useCallback(() => {
+    filterStore.resetFilters(mode, wallet);
+  }, [filterStore, mode, wallet]);
 
   const { error, isLoading, setSize, size, data, mutate } = useGetNFTs(filters);
   const { isLoadingMore, list: items } = useInfiniteScroll({
