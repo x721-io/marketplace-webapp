859a64d8121de37aea4ab20347500195c69342d1
damthang12 <damthangjp1201@gmail.com>
2024-12-04 18:23:52 +0700

    [UPDATE] handle add section

--- components/Org/Overview/add-ov-section-modal.tsx
+++ components/Org/Overview/add-ov-section-modal.tsx
@@ -1,7 +1,27 @@
-"use client";
-
+import React, { useState } from "react";
 import CloseIcon from "@/components/Icon/Close";
 import { Element, ElementType } from "../types";
+import { Dropdown } from "@/components/X721UIKits/Dropdown";
+import Input, { BaseInputProps } from "@/components/Form/Input";
+import {
+  MdOutlineArrowDropDown,
+  MdOutlineArrowDropUp,
+  MdOutlinePermMedia,
+} from "react-icons/md";
+import {
+  elementMediaOnly,
+  elementTextBlockOnly,
+  elementTextLeftMediaRight,
+  elementTextOverBgMedia,
+  elementTextRightMediaLeft,
+} from "@/config/addOnSection";
+import Image from "next/image";
+
+import TextOverBg from "@/public/images/custom-page/TextBackground.png";
+import TextRightMediaLeft from "@/public/images/custom-page/MediaLeftTextRight.png";
+import TextLeftMediaRight from "@/public/images/custom-page/TextLeftMediaRight.png";
+import MediaOnly from "@/public/images/custom-page/BackgroundMedia.png";
+import TextBlockOnly from "@/public/images/custom-page/TextBlock.png";
 
 export default function AddOverviewSectionModal({
   isShow,
@@ -12,384 +32,114 @@ export default function AddOverviewSectionModal({
   onAddNewElement: (element: Element) => void;
   onClose: () => void;
 }) {
+  const [openDropdown, setOpenDropdown] = useState<string | null>(null);
+
+  const categorizedOptions = [
+    {
+      category: "Text and Media",
+      options: [
+        {
+          option: "Text over background media",
+          element: elementTextOverBgMedia,
+          exampleImage: TextOverBg,
+        },
+        {
+          option: "Media on the left and text on the right",
+          element: elementTextRightMediaLeft,
+          exampleImage: TextRightMediaLeft,
+        },
+        {
+          option: "Text on the left and media on the right",
+          element: elementTextLeftMediaRight,
+          exampleImage: TextLeftMediaRight,
+        },
+        {
+          option: "Background media",
+          element: elementMediaOnly,
+          exampleImage: MediaOnly,
+        },
+        {
+          option: "Text block",
+          element: elementTextBlockOnly,
+          exampleImage: TextBlockOnly,
+        },
+      ],
+    },
+    // {
+    //   category: "Media Only",
+    //   options: [
+    //     { option: "Image only", element: elementMediaOnly, exampleImage: MediaOnly },
+    //     { option: "Video only", element: elementMediaOnly, exampleImage: MediaOnly },
+    //   ],
+    // },
+  ];
+
   return (
     <div
       style={{
         right: isShow ? "20px" : "-400px",
         transition: "all 0.25s ease",
       }}
-      className="fixed z-50 top-[50%] -translate-y-[50%] bg-[#212121] text-white rounded-md shadow-md h-[700px] w-[400px] flex flex-col pl-7 pr-2 pb-5"
+      className="fixed z-50 top-[50%] -translate-y-[50%] bg-[#212121] text-white rounded-md shadow-md h-[700px] w-[400px] flex flex-col p-6 gap-5"
     >
-      <div className="w-full h-[80px] font-bold text-heading-xs !text-[20px] tracking-wide flex items-center justify-between pr-5">
+      <div className="w-full py-5 font-bold text-heading-xs !text-[20px] tracking-wide flex items-center justify-between ">
         <div>Add section</div>
         <div onClick={() => onClose()} className="cursor-pointer">
           <CloseIcon height={20} color="white" />
         </div>
       </div>
-      <div className="w-full flex flex-col flex-1 overflow-y-auto pr-5">
-        <div
-          onClick={() => {
-            const element: Element = {
-              type: ElementType.CONTAINER,
-              alignItems: "center",
-              justifyContent: "center",
-              background: "#ffffff",
-              height: "500px",
-              styles: {
-                position: "relative",
-              },
-              children: [
-                {
-                  type: ElementType.CONTAINER,
-                  width: "100%",
-                  height: "600px",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  background: "transparent",
-                  children: [
-                    {
-                      type: ElementType.IMAGE,
-                      src: "https://img.freepik.com/premium-photo/seamless-geometric-pattern-fabric-wallpaper-background-design_955379-17743.jpg?semt=ais_hybrid",
-                      height: "500px",
-                    },
-                  ],
-                },
-                {
-                  type: ElementType.CONTAINER,
-                  width: "100%",
-                  height: "100%",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  background: "transparent",
-                  styles: {
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                  },
-                  children: [
-                    {
-                      type: ElementType.TEXT,
-                      contentType: "TITLE",
-                      text: {
-                        content: "This is a title",
-                        color: "#ffffff",
-                        fontSize: "26px",
-                        fontWeight: 700,
-                        textAlign: "center",
-                      },
-                    },
-                    {
-                      type: ElementType.TEXT,
-                      contentType: "DESCRIPTION",
-                      text: {
-                        content: "This is a description.",
-                        color: "#ffffff",
-                        fontSize: "18px",
-                        fontWeight: 300,
-                        textAlign: "center",
-                      },
-                    },
-                  ],
-                },
-              ],
-            };
-            onAddNewElement(element);
-          }}
-          className="w-full cursor-pointer"
-        >
-          Text over background media
-        </div>
-        <div
-          onClick={() => {
-            const element: Element = {
-              type: ElementType.CONTAINER,
-              background: "transparent",
-              height: "500px",
-              styles: {
-                width: "100%",
-                // height: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                alignContent: "center",
-                padding: "40px 80px",
-                gap: "80px",
-              },
-              children: [
-                {
-                  type: ElementType.CONTAINER,
-                  background: "transparent",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  flexDirection: "column",
-                  styles: {
-                    width: "100%",
-                    height: "100%",
-                  },
-                  children: [
-                    {
-                      type: ElementType.CONTAINER,
-                      background: "transparent",
-                      justifyContent: "center",
-                      flexDirection: "column",
-                      styles: {
-                        width: "100%",
-                        maxWidth: "600px",
-                      },
-                      children: [
-                        {
-                          type: ElementType.TEXT,
-                          contentType: "TITLE",
-                          text: {
-                            textAlign: "left",
-                            content: "What is LayerG?",
-                            color: "#252525",
-                            fontSize: "26px",
-                            fontWeight: 700,
-                          },
-                        },
-                        {
-                          type: ElementType.TEXT,
-                          contentType: "DESCRIPTION",
-                          text: {
-                            textAlign: "left",
-                            content:
-                              "Few entities have shaken up the Web3 landscape as much as the NFT marketplace and aggregator Blur has in the last year. In November of 2022, it began to...",
-                            color: "#6A6A6A",
-                            fontSize: "18px",
-                            fontWeight: 300,
-                          },
-                        },
-                      ],
-                    },
-                  ],
-                },
-                {
-                  type: ElementType.CONTAINER,
-                  background: "transparent",
-                  styles: {
-                    width: "100%",
-                  },
-                  children: [
-                    {
-                      type: ElementType.CONTAINER,
-                      background: "transparent",
-                      styles: {
-                        width: "100%",
-                        maxWidth: "600px",
-                      },
 
-                      children: [
-                        {
-                          type: ElementType.VIDEO,
-                          src: "https://stream.mux.com/701ykhqj7byVeIl0002PlMz1cwQAfmcbfCMolJ54Hy6n1E/high.mp4",
-                          showControls: false,
-                          styles: {
-                            objectFit: "cover",
-                            aspectRatio: 1.5,
-                            width: "100%",
-                            height: "100%",
-                            borderRadius: "16px",
-                          },
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            };
-            onAddNewElement(element);
-          }}
-          className="w-full cursor-pointer"
-        >
-          Text left, media right
-        </div>
-        <div
-          onClick={() => {
-            const element: Element = {
-              type: ElementType.CONTAINER,
-              background: "transparent",
-              height: "500px",
-              styles: {
-                width: "100%",
-                // height: "100%",
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                alignContent: "center",
-                padding: "40px 80px",
-                gap: "80px",
-              },
-              children: [
-                {
-                  type: ElementType.CONTAINER,
-                  background: "transparent",
-                  styles: {
-                    width: "100%",
-                  },
-                  children: [
-                    {
-                      type: ElementType.CONTAINER,
-                      background: "transparent",
-                      styles: {
-                        width: "100%",
-                        maxWidth: "600px",
-                      },
+      <div className="w-full flex flex-col flex-1 overflow-y-auto">
+        {categorizedOptions.map((category, index) => (
+          <div key={index} className="mb-4 w-full">
+            {/* Category Title */}
+            <div
+              className="flex flex-col items-center justify-between cursor-pointer py-2 px-3 bg-gray-800 hover:bg-gray-700 rounded-md w-full gap-2"
+              onClick={() =>
+                setOpenDropdown(
+                  openDropdown === category.category ? null : category.category
+                )
+              }
+            >
+              <div className="flex items-center justify-between rounded-md w-full">
+                <div className="flex items-center gap-2">
+                  <MdOutlinePermMedia size={20} />
+                  <span className="font-bold">{category.category}</span>
+                </div>
+                {openDropdown === category.category ? (
+                  <MdOutlineArrowDropUp size={24} />
+                ) : (
+                  <MdOutlineArrowDropDown size={24} />
+                )}
+              </div>
 
-                      children: [
-                        {
-                          type: ElementType.VIDEO,
-                          src: "https://stream.mux.com/701ykhqj7byVeIl0002PlMz1cwQAfmcbfCMolJ54Hy6n1E/high.mp4",
-                          showControls: false,
-                          styles: {
-                            objectFit: "cover",
-                            aspectRatio: 1.5,
-                            width: "100%",
-                            height: "100%",
-                            borderRadius: "16px",
-                          },
-                        },
-                      ],
-                    },
-                  ],
-                },
-                {
-                  type: ElementType.CONTAINER,
-                  background: "transparent",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  flexDirection: "column",
-                  styles: {
-                    width: "100%",
-                    height: "100%",
-                  },
-                  children: [
-                    {
-                      type: ElementType.CONTAINER,
-                      background: "transparent",
-                      justifyContent: "center",
-                      flexDirection: "column",
-                      styles: {
-                        width: "100%",
-                        maxWidth: "600px",
-                      },
-                      children: [
-                        {
-                          type: ElementType.TEXT,
-                          contentType: "TITLE",
-                          text: {
-                            textAlign: "left",
-                            content: "What is LayerG?",
-                            color: "#252525",
-                            fontSize: "26px",
-                            fontWeight: 700,
-                          },
-                        },
-                        {
-                          type: ElementType.TEXT,
-                          contentType: "DESCRIPTION",
-                          text: {
-                            textAlign: "left",
-                            content:
-                              "Few entities have shaken up the Web3 landscape as much as the NFT marketplace and aggregator Blur has in the last year. In November of 2022, it began to...",
-                            color: "#6A6A6A",
-                            fontSize: "18px",
-                            fontWeight: 300,
-                          },
-                        },
-                      ],
-                    },
-                  ],
-                },
-              ],
-            };
-            onAddNewElement(element);
-          }}
-          className="w-full cursor-pointer"
-        >
-          Text right, media left
-        </div>
-        <div
-          onClick={() => {
-            const element: Element = {
-              type: ElementType.CONTAINER,
-              width: "100%",
-              height: "100%",
-              alignItems: "center",
-              justifyContent: "center",
-              background: "#ffffff",
-              children: [
-                {
-                  type: ElementType.IMAGE,
-                  src: "https://img.freepik.com/premium-photo/seamless-geometric-pattern-fabric-wallpaper-background-design_955379-17743.jpg?semt=ais_hybrid",
-                  width: "100%",
-                  height: "686px",
-                },
-              ],
-            };
-            onAddNewElement(element);
-          }}
-          className="w-full cursor-pointer"
-        >
-          Media only
-        </div>
-        <div
-          onClick={() => {
-            const element: Element = {
-              type: ElementType.CONTAINER,
-              width: "100%",
-              height: "400px",
-              alignItems: "center",
-              justifyContent: "center",
-              background: "#ffffff",
-              children: [
-                {
-                  type: ElementType.CONTAINER,
-                  width: "100%",
-                  height: "100%",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  justifyContent: "center",
-                  background: "transparent",
-                  styles: {
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                  },
-                  children: [
-                    {
-                      type: ElementType.TEXT,
-                      contentType: "TITLE",
-                      text: {
-                        content: "This is a title",
-                        color: "#000000",
-                        fontSize: "26px",
-                        fontWeight: 700,
-                        textAlign: "center",
-                      },
-                    },
-                    {
-                      type: ElementType.TEXT,
-                      contentType: "DESCRIPTION",
-                      text: {
-                        content: "This is a description.",
-                        color: "#000000",
-                        fontSize: "18px",
-                        fontWeight: 300,
-                        textAlign: "center",
-                      },
-                    },
-                  ],
-                },
-              ],
-            };
-            onAddNewElement(element);
-          }}
-          className="w-full cursor-pointer"
-        >
-          Text block only
-        </div>
+              <span className="text-sm text-gray-400">
+                Inspire your community by telling them more about your items and
+                collection via photos, video, and text...
+              </span>
+            </div>
+
+            {/* Dropdown Options */}
+            {openDropdown === category.category && (
+              <div className="mt-2">
+                {category.options.map((option, idx) => (
+                  <div
+                    key={idx}
+                    onClick={() => onAddNewElement(option.element)}
+                    className="flex gap-4 p-3 text-[#6A6A6A] hover:bg-gray-700 text-body-16 rounded-md cursor-pointer flex-col"
+                  >
+                    <Image
+                      src={option.exampleImage}
+                      alt={option.option}
+                      className="w-full h-full rounded-md object-cover"
+                    />
+                    <span>{option.option}</span>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
--- components/Org/Overview/edit-ov-section-modal.tsx
+++ components/Org/Overview/edit-ov-section-modal.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useRef, useState } from "react";
 import { Container, Element, ElementType, Image, Text, Video } from "../types";
 import CloseIcon from "@/components/Icon/Close";
